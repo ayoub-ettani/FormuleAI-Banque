4f5a316995a86ee0a0d6af055bac729718aeb8d2
MALKI <ouafae.malki@capgemini.com>
2026-07-16 12:23:35 +0100

    add clientDTO and ClientMapper

--- BanqueCredit/src/main/java/formulAI/project/bank/banqueCredit/mappers/ClientMapper.java
+++ BanqueCredit/src/main/java/formulAI/project/bank/banqueCredit/mappers/ClientMapper.java
@@ -1,4 +1,12 @@
 package formulAI.project.bank.banqueCredit.mappers;
 
+import formulAI.project.bank.banqueCredit.DTO.ClientDTO;
+import formulAI.project.bank.banqueCredit.models.Client;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ClientMapper {
+
+    ClientDTO toDTO(Client client);
+    Client toEntity(ClientDTO clientDTO);
 }
