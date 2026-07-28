6913b001d0244a7ae2894fee2dd74a851a8c2bfb
MALKI <ouafae.malki@capgemini.com>
2026-07-28 13:29:48 +0100

    save all modifications

--- banquecredit-frontend/src/app/app.routes.ts
+++ banquecredit-frontend/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+
+    //clients
+    {
+        path: 'clients',
+        loadComponent: () => import('./features/client/pages/client-list/client-list').then(m => m.ClientList)
+    },
+    {
+        path: 'clients/new',
+        loadComponent: () => import('./features/client/pages/client-form/client-form').then(m => m.ClientForm)
+    },
+    {
+        path: 'clients/update/:id',
+        loadComponent: () => import('./features/client/pages/client-update-data/client-update-data').then(m => m.ClientUpdateData)
+    },
+
+    //demandes
+    {
+        path: 'demandes',
+        loadComponent: () => import('./features/demandes/pages/demande-list/demande-list').then(m => m.DemandeList)
+    },
+    {
+        path: 'demandes/new',
+        loadComponent: () => import('./features/demandes/pages/demande-form/demande-form').then(m => m.DemandeForm)
+    },
+    {
+        path: 'demandes/details/:id',
+        loadComponent: () => import('./features/demandes/pages/demande-details/demande-details').then(m => m.DemandeDetails)
+    }
+
+];
